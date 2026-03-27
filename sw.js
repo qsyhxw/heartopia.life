0ec675c0041ba27599fcb72faa6bba6e61030b72
qsyhxw <qsyhxw@gmail.com>
2026-03-27 11:54:09 +0800

    更换广告标签

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10769074
+    "zoneId": 10791739
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
